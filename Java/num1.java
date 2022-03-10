bc555cb2ec9e321a7c557f7f9e8e8d5c859fcf2b
Ghost1606 <101306645+Ghost1606@users.noreply.github.com>
2022-03-11 00:49:43 +0900

    Add files via upload

---
+++ Java/num1.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Scanner;
+
+public class num1 {
+    public static void main(String[] args) {
+        
+        int a,b;
+        int s;
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("두 정수를 입력하세요 : ");
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        s = a+b;
+
+        System.out.println(a+"+"+b+"은\t"+s);
+
+        sc.close();
+    }
+}
+
---
+++ Java/num2.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Scanner;
+
+public class num2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("몇 층인지 입력하세요 : ");
+
+        int a = sc.nextInt();
+        int h = (a * 5);
+
+        System.out.println(h+"m"+"입니다.");
+
+        sc.close();
+    }
+}
