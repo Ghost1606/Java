620f439353b80b5f25319c4d2752011f0758d944
Ghost1606 <101306645+Ghost1606@users.noreply.github.com>
2022-03-14 19:11:13 +0900

    Add files via upload

--- Java/num6.java
+++ Java/num6.java
@@ -12,17 +12,65 @@ public class num6 {
 		
 		int a = sc.nextInt();
 		
-		if()
+		int oman,man,chen,obeak,beak,sib,eli;
+		
+		oman = a / 50000;
+		man = oman % 50000 / 10000;
+		chen = man % 50000 % 10000 / 1000;
+		obeak = chen % 50000 % 10000 % 1000 / 500;
+		beak = obeak % 50000 % 10000 % 1000 % 500 / 100;
+		sib = beak  % 50000 % 10000 % 1000 % 500 % 100 / 10;
+		eli = sib  % 50000 % 10000 % 1000 % 500 % 100 % 10 / 1;
+		
+		if(a >= 50000)
 		{
-			
+			System.out.println("오만원"+oman+"개");
 		}
 		else
 		{
-			
+			if(a > 10000)
+			{
+				System.out.println("만원"+man+"개");
+			}
+			else
+			{
+				if(a > 1000)
+				{
+					System.out.println("천원"+chen+"개");
+				}
+				else
+				{
+					if(a > 500)
+					{
+						System.out.println("500원"+obeak+"개");
+					}
+					else
+					{
+						if(a > 100)
+						{
+							System.out.println("100원"+beak+"개");
+						}
+						else
+						{
+							if(a > 10)
+							{
+								System.out.println("10원"+sib+"개");
+							}
+							else
+							{
+								if(a > 1)
+								{
+									System.out.println("1원"+eli+"개");
+								}
+							}
+						}
+					}
+				}
+			}
 		}
 		
-		
-		sc.close();
+
 	}
-	
 }
+	
+
